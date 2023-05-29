befa1b325b68c821bfdb8106bc5cad1aacadbbe5
Sourav Majumder <souravmajumderk99@gmail.com>
2023-05-29 17:51:11 +0600

    done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,32 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import {
+  configureChains,
+  WagmiConfig,
+  createClient,
+  sepolia,
+  createConfig,
+} from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { SessionProvider } from "next-auth/react";
+
+const { publicClient, webSocketPublicClient } = configureChains(
+  [sepolia],
+  [publicProvider()]
+);
+
+const config = createConfig({
+  publicClient,
+  webSocketPublicClient,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <WagmiConfig config={config}>
+        <SessionProvider session={pageProps.session} refetchInterval={0}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </WagmiConfig>
+    </>
+  );
 }
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth";
+import { MoralisNextAuthProvider } from "@moralisweb3/next";
+
+export default NextAuth({
+  providers: [MoralisNextAuthProvider()],
+  // adding user info to the user session object
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = user;
+      }
+      console.log("token", token);
+      return token;
+    },
+    async session({ session, token }) {
+      session.user = token.user;
+      console.log("session", session);
+      return session;
+    },
+  },
+});
